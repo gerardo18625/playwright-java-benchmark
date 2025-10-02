18466962b6528d97af67f97038b00aca2930bf42
Gerardo Hernandez <ghernandez@crunchtime.com>
2025-10-02 16:14:17 -0600

    Changing to 30 workers and 100 tests

--- src/test/java/com/crunchtime/ctAutomation/TestHeroku.java
+++ src/test/java/com/crunchtime/ctAutomation/TestHeroku.java
@@ -13,7 +13,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 public class TestHeroku {
 
 
-    @Test(invocationCount = 20, threadPoolSize = 20)
+    @Test(invocationCount = 100, threadPoolSize = 30)
     public void herokuTest(){
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
