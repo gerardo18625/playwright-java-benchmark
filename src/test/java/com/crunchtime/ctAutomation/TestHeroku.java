0aa3de7dc6d863bde72f5752211e7ffdd646fa07
Gerardo Hernandez <ghernandez@crunchtime.com>
2025-10-02 15:27:18 -0600

    Changing to 9 workers

--- src/test/java/com/crunchtime/ctAutomation/TestHeroku.java
+++ src/test/java/com/crunchtime/ctAutomation/TestHeroku.java
@@ -13,7 +13,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 public class TestHeroku {
 
 
-    @Test(invocationCount = 20, threadPoolSize = 6)
+    @Test(invocationCount = 20, threadPoolSize = 9)
     public void herokuTest(){
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
